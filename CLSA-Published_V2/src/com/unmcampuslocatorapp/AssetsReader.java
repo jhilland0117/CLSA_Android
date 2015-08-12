f5051dca81e7329a5a18a701722ee1f2a998dd7d
jhilland0117 <jhilland@unm.edu>
2015-08-12 08:32:50 -0600

    Version 2
    Added version 2 current published on Google Play Store

---
+++ CLSA-Published_V2/src/com/unmcampuslocatorapp/AssetsReader.java
@@ -0,0 +1,44 @@
+package com.unmcampuslocatorapp;
+/*
+ * class AssetReader used for abqbuildings.txt. needs to be modified to support all
+ * text files we will be using (easy task). all objects set as string type.
+ * perhaps change this rather than converting later on??
+ */
+
+
+
+public class AssetsReader {
+	
+	public String title;
+	public String latitude;
+	public String longitude;
+	public String buildingAbbr;
+
+	public String getBuildingAbbr() {
+		return buildingAbbr;
+	}
+	public void setBuildingAbbr(String buildingAbbr) {
+		this.buildingAbbr = buildingAbbr;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	
+}
---
+++ CLSA-Published_V2/src/com/unmcampuslocatorapp/ZoomPicture.java
@@ -0,0 +1,15 @@
+package com.unmcampuslocatorapp;
+
+import com.unmcampuslocatorapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ZoomPicture extends Activity {
+@Override
+public void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.zoom_pic2);
+	}
+}
